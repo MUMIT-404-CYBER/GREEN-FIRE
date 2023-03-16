fa577a09521911082ce80764288834fe1e1c65ab
INNOCENT BOY <119349078+MUMIT-404-CYBER@users.noreply.github.com>
2023-03-16 22:12:40 +0600

    Update fire.py

--- fire.py
+++ fire.py
@@ -1,7 +1,8 @@
 import os, sys
 print("")
-print("")
+os.system('clear')
 print("   Update Checking...")
+time.sleep(1.0)
 print("   Not Updated !!")
+time.sleep(1.0)
 print("   Update Coming Soon...")
-print("   Love U All Dear User.")
